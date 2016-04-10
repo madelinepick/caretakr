cc8c8468815ab737c5f7925b8cd61574441f901a
Madeline Pickering <madelinepickering@gmail.com>
2016-04-10 14:55:29 -0700

    basic app page set up

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+// Eventually put user name and kid's name req param here ==>
+router.get('/app/username/kidsname', function(req, res, next) {
+  res.render('app');
 });
 
 module.exports = router;
