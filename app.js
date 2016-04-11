656de3e074e7bc98d2b0531be50f4c7c8febb83b
MSturges <Sturges3232@gmail.com>
2016-04-11 11:37:40 -0600

    google oauth return objects

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var GoogleStrategy = require('passport-google-oauth2').Strategy
 var passport = require('passport');
 var cookieSession = require('cookie-session');
+var dotenv = require('dotenv');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
