fd33fdf10e221dd2c920731f86562181aed288fd
Alan Taylor <alancarltaylor@gmail.com>
2016-04-11 16:06:25 -0600

    changed form on add.hbs, router.post added for that page

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:user_id/add', function(req, res, next){
-  res.render('add');
+  res.render('add', {user_id: req.params.user_id});
 });
+
+router.post('/:user_id/add', function(req, res, next){
+  return knex('dependents')
+    .insert({
+      name: req.body.name,
+      being_type: req.body.being_type,
+      picture_url: req.body.picture_url,
+      contact_info_id: req.body.contact_info_id,
+      user_id: req.params.user_id
+    }).then(function() {
+      res.redirect('/:user_id/home')
+    })
+})
+
+
 
 router.get('/:user_id/update', function(req, res, next){
   res.render('update');
