648950b26f435ac38727cd080813304c4e042242
Alan Taylor <alancarltaylor@gmail.com>
2016-04-12 10:16:07 -0600

    fixed redirect link for add, corrected keynames for the post request

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('knex');
+var knex = require('knex')(require('../knexfile')['development']);
 
 router.get('/:user_id/add', function(req, res, next){
   res.render('add', {user_id: req.params.user_id});
@@ -9,19 +9,24 @@ router.get('/:user_id/add', function(req, res, next){
 router.post('/:user_id/add', function(req, res, next){
   return knex('dependents')
     .insert({
-      name: req.body.name,
+      dependent_name: req.body.name,
       being_type: req.body.being_type,
       picture_url: req.body.picture_url,
       contact_info_id: req.body.contact_info_id,
       user_id: req.params.user_id
     }).then(function() {
-      res.redirect('/:user_id/home')
+      res.redirect('/admin/'+req.params.user_id+'/home')
     })
 })
 
 
 
-router.get('/:user_id/update', function(req, res, next){
+router.get('/:user_id/update/:dependents_id', function(req, res, next){
+  return knex('dependents')
+  .where({user_id: req.params.user_id})
+  .then(function(data){
+    console.log(data)
+  })
   res.render('update');
 });
 
