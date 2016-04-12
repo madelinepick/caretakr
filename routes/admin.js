ee0826ef2da28c21b3f356e6a9d2abc84fbe9112
Alan Taylor <alancarltaylor@gmail.com>
2016-04-12 14:28:53 -0600

    ran a new migration to include vet_number

--- routes/admin.js
+++ routes/admin.js
@@ -8,14 +8,11 @@ router.get('/:user_id/add', function(req, res, next) {
       user_id: req.params.user_id
     })
     .then(function(data) {
-
-      //console.log(data)
-
       res.render('add', {
         dependents: data,
         user_id: req.params.user_id
       })
-  });
+    });
 });
 
 router.post('/:user_id/add', function(req, res, next) {
@@ -51,37 +48,41 @@ router.get('/:user_id/update/:dependents_id', function(req, res, next) {
               user_id: req.params.user_id
             })
             .then(function(even_more_data) {
-          res.render('update', {
-            dependent_data: data,
-            rules_data: more_data,
-            dependents: even_more_data,
-            user_id: req.params.user_id
-          });
+              res.render('update', {
+                dependent_data: data,
+                rules_data: more_data,
+                dependents: even_more_data,
+                user_id: req.params.user_id
+              });
+            });
         });
-    });
-  })
+    })
 });
 
-router.post('/:user_id/update/:dependents_id', function(req, res, next){
+router.post('/:user_id/update/:dependents_id', function(req, res, next) {
   return knex('dependents')
-    .where({dependents_id: req.params.dependents_id})
+    .where({
+      dependents_id: req.params.dependents_id
+    })
     .update({
       dependent_name: req.body.dependent_name,
       being_type: req.body.being_type,
       picture_url: req.body.picture_url,
       contact_info_id: req.body.contact_info_id,
       user_id: req.params.user_id
-    }).then(function(){
+    }).then(function() {
       console.log(req.body.dependent_name)
       res.redirect('/admin/' + req.params.user_id + '/home')
     })
 });
 
-router.post('/:user_id/delete/:dependents_id', function(req, res, next){
+router.post('/:user_id/delete/:dependents_id', function(req, res, next) {
   return knex('dependents')
-    .where({dependents_id: req.params.dependents_id})
+    .where({
+      dependents_id: req.params.dependents_id
+    })
     .del()
-    .then(function(){
+    .then(function() {
       res.redirect('/admin/' + req.params.user_id + '/home')
     })
 })
@@ -92,15 +93,24 @@ router.get('/:user_id/home', function(req, res, next) {
       user_id: req.params.user_id
     })
     .then(function(data) {
-
-      //console.log(data)
-
       res.render('home', {
         dependents: data,
         user_id: req.params.user_id
       });
     })
-})
+});
+
+router.get('/:user_id/contacts', function(req, res, next){
+  return knex('dependents')
+    .where({
+      user_id: req.params.user_id
+    })
+    .then(function(data) {
+      res.render('contact', {
+        dependents: data,
+        user_id: req.params.user_id
+      });
+    })})
 
 
 
