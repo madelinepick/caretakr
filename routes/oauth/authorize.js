30f6f7b4027e2fbe8938d1ec2ee5cca7c8e96a8e
Nathan Reginato <nathan.reginato@gmail.com>
2016-04-13 13:17:29 -0600

    Login auth works now

--- routes/oauth/authorize.js
+++ routes/oauth/authorize.js
@@ -19,7 +19,13 @@ module.exports = {
         });
       })
     },
-    loggedIn: function() {
-      
-    }
+    
+    loggedIn: function(req, res, next) {
+        // console.log(req.session.passport.user.photos);
+        if (req.session.passport) {
+          next();
+        } else {
+          res.redirect('/');
+        }
+      }
   }
