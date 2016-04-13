b7bfe5a9faa8649ce8f513d2434195dbe87382d6
Alan Taylor <alancarltaylor@gmail.com>
2016-04-13 15:21:46 -0600

    as an admin, clicking on a dependent on the left takes you to a display page, instead of directly to an edit page

--- routes/admin.js
+++ routes/admin.js
@@ -168,4 +168,36 @@ router.post('/:user_id/contacts', function(req, res, next){
     })
   })
 
+  router.get('/:user_id/dependents/:dependents_id', function(req, res, next){
+    return knex('dependents')
+    .where({user_id: req.params.user_id})
+    .first()
+    .then(function(data) {
+      return knex('dependents')
+      .where({dependents_id: req.params.dependents_id})
+      .first()
+      .then(function(rules_data){
+        return knex('contact_info')
+        .where({user_id: req.params.user_id})
+        .first()
+        .then(function(contact_info_data){
+          return knex('users')
+          .where({user_id: req.params.user_id})
+          .first()
+          .then(function(user_data){
+
+console.log(data);
+        res.render('dependents', {
+          contact_info: contact_info_data,
+          user: user_data,
+          dependents: data,
+          rules: rules_data,
+          user_id: req.params.user_id
+        })
+        })
+        })
+      })
+    });
+  })
+
 module.exports = router;
