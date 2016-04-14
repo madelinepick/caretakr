bf123c48360d5249d3b0ae4850cb8d13303d9079
Alan Taylor <alancarltaylor@gmail.com>
2016-04-14 17:52:27 -0600

    public landing page created

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,48 @@ router.get('/', function(req, res, next) {
 });
 
 /*Public Route.*/
+
 router.get('/:user_id/public/:dependents_id', function(req, res, next){
+  var dependentsArray = []
+  return knex ('contact_info')
+  .where({user_id: req.params.user_id})
+  .first()
+  .then(function(contact_info_data){
+
+  return knex ('users')
+  .where({user_id: req.params.user_id})
+  .first()
+  .then(function(user_data){
+
+  return knex('dependents')
+  .where({user_id: req.params.user_id})
+  .then(function(user_dependents){
+    return knex('dependents')
+    .where({dependents_id: req.params.dependents_id})
+    .first()
+    .then(function(specific_dependent){
+      specific_dependent.title.titles.forEach(function(elem, index){
+        dependentsArray.push({
+                              title: elem,
+                              body: specific_dependent.title.body[index]
+                            })
+      })
+      res.render('public_dependents', {
+        user_name: user_data.user_name.substring(0, user_data.user_name.indexOf("@")),
+        user: user_data,
+        contact_info: contact_info_data,
+        dependents: user_dependents,
+        dependent_id: specific_dependent.dependents_id,
+        rules: dependentsArray,
+        user_id: req.params.user_id
+      })
+    })
+    })
+  });
+})
+})
+
+router.get('/:user_id/public/', function(req, res, next){
   return knex('dependents')
   .where({user_id: req.params.user_id})
   .then(function(data) {
