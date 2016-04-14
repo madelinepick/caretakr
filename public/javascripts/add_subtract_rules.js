68c745f6359cc9d6cbf7e7fdb7f773dc57ea7f58
Nathan Reginato <nathan.reginato@gmail.com>
2016-04-14 12:30:20 -0600

    Dependents view works

---
+++ public/javascripts/add_subtract_rules.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('#add_rule').on('click', function() {
+    $('#add_rules_form').append('<label for="section1name">Rule Name:</label><input type="text" name="title" value=""><br><label for="section1">Content</label><br><textarea name="body" rows="4" cols="40"></textarea><br>')
+  })
+})
