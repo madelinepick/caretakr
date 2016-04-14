a36342474453c3022a8f057d04eeb667a4d4495d
Nathan Reginato <nathan.reginato@gmail.com>
2016-04-14 15:18:10 -0600

    Delete works again

--- routes/admin.js
+++ routes/admin.js
@@ -52,41 +52,34 @@ router.post('/:user_id/add/', function(req, res, next) {
 })
 
 router.get('/:user_id/update/:dependents_id', function(req, res, next) {
+  var dependentsArray = []
   authorized.fun(req).then(function(){
     return knex('dependents')
-    .where({ dependents_id: req.params.dependents_id })
-    .first()
-    .then(function(data) {
-        return knex('dependents')
-        .where({ user_id: req.params.user_id })
-        .then(function(even_more_data) {
-          return  knex('users')
-          .where({user_id: req.params.user_id})
-          .first()
-          .then(function(user_data){
+    .where({user_id: req.params.user_id})
+    .then(function(user_dependents){
+      return knex('dependents')
+        .where({dependents_id: req.params.dependents_id})
+        .first()
+        .then(function(specific_dependent){
+          specific_dependent.title.titles.forEach(function(elem, index){
+            dependentsArray.push({
+                                  title: elem,
+                                  body: specific_dependent.title.body[0]
+                                })
+          })
+          console.log(dependentsArray);
           res.render('update', {
-            title1: data.title.titles[0],
-            title2: data.title.titles[1],
-            title3: data.title.titles[2],
-            title4: data.title.titles[3],
-
-            body1: data.title.body[0],
-            body2: data.title.body[1],
-            body3: data.title.body[2],
-            body4: data.title.body[3],
-
-            user: user_data,
-
-            dependent_data: data,
-            dependents: even_more_data,
+            dependents: user_dependents,
+            dependent_id: specific_dependent.dependents_id,
+            rules: dependentsArray,
             user_id: req.params.user_id
           })
-          });
-        });
+        })
       });
-    }).catch(function() {
-      res.redirect('/');
     })
+    // .catch(function() {
+    //   res.redirect('/');
+    // })
   });
 
 router.post('/:user_id/update/:dependents_id', function(req, res, next) {
@@ -223,9 +216,9 @@ router.post('/:user_id/contacts', function(req, res, next){
                                 body: specific_dependent.title.body[0]
                               })
         })
-        console.log(dependentsArray);
         res.render('dependents', {
           dependents: user_dependents,
+          dependent_id: specific_dependent.dependents_id,
           rules: dependentsArray,
           user_id: req.params.user_id
         })
